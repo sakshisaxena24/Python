36c7101dace9c3d5181c20d7a5a9b87e58d30d21
sakshisaxena24 <71274782+sakshisaxena24@users.noreply.github.com>
2021-11-18 17:43:41 +0530

    day 1 and 2 projects

---
+++ day 2/python_day2_typeconversion.py
@@ -0,0 +1,9 @@
+two_digit_number = input("Type a two digit number: ")
+
+first_digit= two_digit_number[0]
+type(first_digit)
+second_digit= two_digit_number[1]
+
+##type conversion##
+result= int(first_digit)+int(second_digit)
+print(result)
