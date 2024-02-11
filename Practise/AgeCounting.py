7459f8773ca0dd0521aafc10c2f13417a513cf2b
sakshisaxena24 <71274782+sakshisaxena24@users.noreply.github.com>
2024-02-11 17:13:00 -0600

    Delete Practise/AgeCounting.py

--- Practise/AgeCounting.py
+++
@@ -1,8 +0,0 @@
-import requests
-import sys
-from io import StringIO
-import numpy as np
-import pandas as pd
-
-r = requests.get('https://coderbyte.com/api/challenges/json/age-counting')
-raw_v = r.json()['data']
